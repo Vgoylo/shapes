388c3d1e17c55e64925b711ff324d85ad1be6dac
vgoylo <y_draiv.by@mail.ru>
2021-07-23 16:40:46 +0300

    Allow user to calculate triangle, rectangle square from input

--- run.rb
+++ run.rb
@@ -3,14 +3,8 @@ require_relative 'src/circle'
 require_relative 'src/rectangle'
 # triangle = Triangle.new(5, 5)
 # puts "triangle square - #{triangle.square}"
-
-
-
 # rectangle = Rectangle.new(5, 5)
 # puts "rectangle square - #{rectangle.square}"
-
-
-
 # circle = Circle.new(5)
 # puts "circle square - #{circle.square}"
 
@@ -25,7 +19,12 @@ puts '3. rectangle'
 shape = gets.chomp
 
 if shape == '1'
-  puts 'triangle'
+  puts 'Please, enter the triangle side: '
+  side = gets.chomp.to_i
+  puts 'Please, enter the triangle height: '
+  height = gets.chomp.to_i
+  triangle = Triangle.new(side, height)
+  puts "triangle square - #{triangle.square}"
 
 elsif shape == '2'
   puts 'Please, enter the circle radius: '
@@ -34,8 +33,17 @@ elsif shape == '2'
   puts "circle square - #{circle.square}"
 
 elsif shape == '3'
-  puts 'rectangle'
-
+  puts 'Please, enter the rectangle long: '
+  long = gets.chomp.to_i
+  puts 'Please, enter the rectangle width: '
+  width = gets.chomp.to_i
+  rectangle = Rectangle.new(long, width)
+  puts "rectangle square - #{rectangle.square}"
+ 
 elsif 
   puts 'Invalid inpud'
 end
+
+
+
+
