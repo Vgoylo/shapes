55a463dfb2f42296140458020babe058fb2677c5
vgoylo <y_draiv.by@mail.ru>
2021-07-29 15:24:45 +0300

    Create method for printing square

--- run.rb
+++ run.rb
@@ -5,20 +5,25 @@ require_relative 'src/circle'
 require_relative 'src/rectangle'
 require_relative 'src/square'
 
+def print_square(shape)
+  result = shape.square
+  puts "#{shape.class} square - #{result}"
+end
+
 def triangle
   puts 'Please, enter the triangle side: '
   side = gets.chomp.to_i
   puts 'Please, enter the triangle height: '
   height = gets.chomp.to_i
   triangle = Triangle.new(side, height)
-  puts "triangle square - #{triangle.square}"
+  print_square(triangle)
 end
 
 def circle
   puts 'Please, enter the circle radius: '
   radius = gets.chomp.to_i
   circle = Circle.new(radius)
-  puts "circle square - #{circle.square}"
+  print_square(circle)
 end
 
 def rectangle
@@ -27,14 +32,14 @@ def rectangle
   puts 'Please, enter the rectangle width: '
   width = gets.chomp.to_i
   rectangle = Rectangle.new(long, width)
-  puts "rectangle square - #{rectangle.square}"
+  print_square(rectangle)
 end
 
 def square
   puts 'Please, enter the square long: '
   long = gets.chomp.to_i
   square = Square.new(long)
-  puts "square square - #{square.square}"
+  print_square(square)
 end
 
 puts 'select the area of which figure you want to calculate: '
