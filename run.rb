7d2298738a00118e4a1ebd366ead7163f7c46cd6
vgoylo <y_draiv.by@mail.ru>
2021-07-29 14:30:14 +0300

    Divide to method

--- run.rb
+++ run.rb
@@ -11,30 +11,46 @@ puts '4. square'
 
 shape = gets.chomp
 
-if shape == '1'
+def triangle
   puts 'Please, enter the triangle side: '
   side = gets.chomp.to_i
   puts 'Please, enter the triangle height: '
   height = gets.chomp.to_i
   triangle = Triangle.new(side, height)
   puts "triangle square - #{triangle.square}"
-elsif shape == '2'
+end
+
+def circle
   puts 'Please, enter the circle radius: '
   radius = gets.chomp.to_i
   circle = Circle.new(radius)
   puts "circle square - #{circle.square}"
-elsif shape == '3'
+end
+
+def rectangle
   puts 'Please, enter the rectangle long: '
   long = gets.chomp.to_i
   puts 'Please, enter the rectangle width: '
   width = gets.chomp.to_i
   rectangle = Rectangle.new(long, width)
   puts "rectangle square - #{rectangle.square}"
-elsif shape == '4'
+end
+
+def square
   puts 'Please, enter the square long: '
   long = gets.chomp.to_i
   square = Square.new(long)
   puts "square square - #{square.square}"
+
+end
+if shape == '1'
+  triangle
+elsif shape == '2'
+  circle
+elsif shape == '3'
+  rectangle
+elsif shape == '4'
+  square
 else
   puts 'Invalid inpud'
 end
